4d62684c9c452c2d8f1a460af865a091416e772a
Avery Berkowitz <avery.berkowitz@gmail.com>
2019-11-01 10:42:04 -0500

    create post route to save user to db

--- src/server/routes.js
+++ src/server/routes.js
@@ -2,12 +2,32 @@ const { Router } = require('express');
 
 const routes = Router();
 
+// require models
+const { User, Pothole } = require('./db/index');
+
 routes.post('/potholes', (req, res) => {
   console.log('boom');
 });
 
 routes.get('/potholes', (req, res) => {
   console.log('WORKED');
+});
+
+// post route to create a user
+routes.post('/users', (req, res) => {
+  // save user to db
+  // hardcoded user for testing
+  User.create({
+    full_name: 'Avery',
+    email: 'avery.berkowitz@gmail.com',
+  })
+    .then(() => {
+      res.send(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send(500);
+    });
 });
 
 module.exports = { routes };

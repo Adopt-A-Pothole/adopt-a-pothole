310180519211c8bb9dc2aef250edda5c509ef7c6
Avery Berkowitz <avery.berkowitz@gmail.com>
2019-11-01 16:18:46 -0500

    create handler functions for inputs

--- src/client/components/CreatePothole.js
+++ src/client/components/CreatePothole.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CreatePothole extends Component {
   constructor(props) {
@@ -9,20 +10,68 @@ export default class CreatePothole extends Component {
       description: null,
       image: null,
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleAddress = this.handleAddress.bind(this);
+    this.handleDescription = this.handleDescription.bind(this);
+    this.handleImage = this.handleImage.bind(this);
+  }
+
+  // handle form submit
+  handleSubmit() {
+    // send post request to /potholes
+  }
+
+  // handle address change
+  handleAddress(event) {
+    const address = event.target.value;
+    this.setState({
+      address,
+    });
+  }
+
+  // handle severity score
+
+  // handle description change
+  handleDescription(event) {
+    const description = event.target.value;
+    this.setState({
+      description,
+    });
+  }
+
+  // handle image change
+  handleImage(event) {
+    const image = event.target.value;
+    this.setState({
+      image,
+    });
   }
 
   render() {
+    const {
+      handleSubmit,
+      handleAddress,
+      handleDescription,
+      handleImage,
+    } = this;
     return (
       <div>
+        <hr />
         <h1> Create Pothole </h1>
         <h3>Type in Address of Pothole</h3>
-        <input type="text" />
+        <input type="text" onChange={handleAddress} />
         <h3>How Bad is it?</h3>
-        <button>Small</button>
-        <button>Medium</button>
-        <button>Big as Hell</button>
+        <button type="submit">Small</button>
+        <button type="submit">Medium</button>
+        <button type="submit">Big as Hell</button>
         <h3>Picture url</h3>
-        <input type="text"/>
+        <input type="text" onChange={handleImage} />
+        <h3>Description</h3>
+        <input type="text" onChange={handleDescription} />
+        <div>
+          <button onClick={handleSubmit} type="submit">Report Pothole</button>
+        </div>
+        <hr />
       </div>
     );
   }

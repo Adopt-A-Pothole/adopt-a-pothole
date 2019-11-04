5d9e21b33aa16d415e0f22027731ea30b05ef569
Titus Charles <tcharles23.tc@gmail.com>
2019-11-04 15:09:06 -0600

    use helper function to in routes to save user & pothole

--- src/server/routes.js
+++ src/server/routes.js
@@ -4,20 +4,26 @@ const routes = Router();
 
 // require models
 const { User, Pothole } = require('./db/index');
+const { saveUser, savePothole } = require('./db/helpers');
+
 
 routes.post('/potholes', (req, res) => {
-  Pothole.create({
-    longitude: 29.9511,
-    latitude: 90.0715,
-    severity: 10,
-    description: 'Its BIG!',
-    fill_cost: 10000,
-    money_donated: 100.00,
-    filled: false,
-    image: ':)',
+  console.log(req);
+  savePothole(req.body);
+  res.sendStatus(200);
+  res.end();
+});
+
+// a get route to get a pothole
+routes.get('/potholes', (req, res) => {
+  console.log('WORKED');
+  return Pothole.findAll({
+    // where: { severity: 10 }
   })
-    .then(() => {
-      res.send(201);
+    .then((pothole) => {
+      console.log(pothole);
+      res.send(pothole);
+      res.end();
     })
     .catch((err) => {
       console.error(err);
@@ -25,25 +31,29 @@ routes.post('/potholes', (req, res) => {
     });
 });
 
-routes.get('/potholes', (req, res) => {
-  console.log('WORKED');
+// get route to get a user
+routes.get('/users', (req, res) => {
+  // save user to db
+  // hardcoded user for testing
+  User.findAll({
+    full_name: 'Avery',
+  })
+    .then((user) => {
+      res.send(user);
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send(500);
+    });
 });
-
 // post route to create a user
 routes.post('/users', (req, res) => {
   // save user to db
-  // hardcoded user for testing
-  User.create({
-    full_name: 'Avery',
-    email: 'avery.berkowitz@gmail.com',
-  })
-    .then(() => {
-      res.send(201);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.send(500);
-    });
+  saveUser(req.body);
+  res.sendStatus(201);
+  res.end();
 });
+
 
 module.exports = { routes };

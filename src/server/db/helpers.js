07406d2be98cc2ffad6df3b3c8894ec0ae461b75
Titus Charles <tcharles23.tc@gmail.com>
2019-11-04 15:08:23 -0600

    add a help function to save user & pothole to db

--- src/server/db/helpers.js
+++ src/server/db/helpers.js
@@ -1,2 +1,30 @@
 // require models
+
 const { User, Pothole } = require('./index');
+
+// save user to the database
+const saveUser = req => User.create({
+  id: req.id,
+  full_name: req.full_name,
+  email: req.email,
+  createdAt: req.createAt,
+  updatedAt: req.updatedAt
+});
+
+
+// save pothole to the database
+const savePothole = req => Pothole.create({
+  longitude: req.longitude,
+  latitude: req.latitude,
+  severity: req.severity,
+  description: req.description,
+  fill_cost: req.fill_cost,
+  money_donated: req.money_donated,
+  filled: req.filled,
+  image: req.image,
+  createdAt: req.createdAt
+});
+
+
+module.exports.savePothole = savePothole;
+module.exports.saveUser = saveUser;

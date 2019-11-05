6d6663ff439bd91afc60de07bff168e57ad7af2f
Jade <jadejdoucet@gmail.com>
2019-11-05 12:26:35 -0600

    added helpers and route to get /pothole

--- src/server/db/helpers.js
+++ src/server/db/helpers.js
@@ -1,2 +1,36 @@
-// require models
-const { User, Pothole } = require('./index');
+const { User, Pothole, Donation } = require('./index');
+
+// save user to the database
+const saveUser = req => User.create({
+    id: req.id,
+    full_name: req.full_name,
+    email: req.email,
+    amount: req.amount,
+    createdAt: req.createAt,
+    updatedAt: req.updatedAt
+});
+
+
+// save pothole to the database
+const savePothole = req => Pothole.create({
+    longitude: req.longitude,
+    latitude: req.latitude,
+    severity: req.severity,
+    description: req.description,
+    fill_cost: req.fill_cost,
+    money_donated: req.money_donated,
+    filled: req.filled,
+    image: req.image,
+    createdAt: req.createdAt
+});
+
+const saveDonation = req => Donation.create({
+    id: req.id,
+    amount: req.amount,
+    email: req.email,
+    user_id: req.id
+});
+
+module.exports.saveDonation = saveDonation;
+module.exports.savePothole = savePothole;
+module.exports.saveUser = saveUser;

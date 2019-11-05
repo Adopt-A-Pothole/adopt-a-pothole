efdedf27118f522e1e4ea2bf6f9dcfe33b2f6544
Titus Charles <tcharles23.tc@gmail.com>
2019-11-05 11:31:43 -0600

    merge changes

--- src/client/index.js
+++ src/client/index.js
@@ -3,8 +3,31 @@ import ReactDOM from 'react-dom';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 // semantic ui import
 import 'semantic-ui-css/semantic.min.css';
+import { Menu, Header } from 'semantic-ui-react';
 
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <div>
+      <Menu>
+        <Menu.Item>
+          <Header as="h1">Adopt A Pothole</Header>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to="/create">Add A Pothole</Link>
+        </Menu.Item>
+      </Menu>
+      <Link id="CreatePothole" to="/create">Add A Pothole</Link>
+      <Route exact path="/" component={App} />
+      <Route path="/create" component={CreatePothole} />
+      <Route path="/pothole" component={Pothole} />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,6 +1,21 @@
 const { Router } = require('express');
+require('dotenv').config();
 
 const routes = Router();
+
+// paypal config
+const paypal = require('paypal-rest-sdk');
+
+const {
+  paypal_client_id,
+  paypal_client_secret,
+} = process.env;
+
+paypal.configure({
+  mode: 'sandbox', // Sandbox or live
+  client_id: paypal_client_id,
+  client_secret: paypal_client_secret
+});
 
 // require models
 const { User, Pothole } = require('./db/index');
@@ -61,6 +76,89 @@ routes.post('/users', (req, res) => {
     .catch((err) => {
       console.log(err, 'errr');
     });
+});
+
+
+// post route to make a paypal payment
+routes.post('/donate', (req, res) => {
+  // get payment amount
+  const { donation } = req.body;
+  // make sure donation is valid
+  if (isNaN(+donation) || +donation < 0) {
+    console.log('not a valid amount');
+    res.send('invalid');
+    return;
+  }
+  // create payment object
+  const create_payment_json = {
+    intent: 'sale',
+    payer: {
+      payment_method: 'paypal'
+    },
+    redirect_urls: {
+      return_url: 'http://localhost:8080/success',
+      cancel_url: 'http://localhost:8080/cancel'
+    },
+    transactions: [{
+      item_list: {
+        items: [{
+          name: 'big hole',
+          sku: '001',
+          currency: 'USD',
+          price: donation,
+          quantity: 1
+        }]
+      },
+      amount: {
+        currency: 'USD',
+        total: donation
+      },
+      description: 'This is the payment description.'
+    }]
+  };
+  // send payment object to paypal
+  paypal.payment.create(create_payment_json, (error, payment) => {
+    if (error) {
+      throw error;
+    } else {
+      // find approval url
+      for (let i = 0; i < payment.links.length; i++) {
+        if (payment.links[i].rel === 'approval_url') {
+          // send back url
+          res.send(payment.links[i].href);
+          // res.redirect(payment.links[i].href);
+        }
+      }
+    }
+  });
+});
+
+// handle successful payment
+routes.get('/success', (req, res) => {
+  const payerId = req.query.PayerID;
+  const { paymentId } = req.query;
+
+  const execute_payment_json = {
+    payer_id: payerId,
+  };
+
+  paypal.payment.execute(paymentId, execute_payment_json, (error, payment) => {
+    if (error) {
+      console.log(error.response);
+      throw error;
+    } else {
+      console.log(JSON.stringify(payment));
+      // TODO need to save transaction to db;
+      // TODO prompt a toast saying successful payment
+      res.redirect('/');
+    }
+  });
+});
+
+// send toast that transaction was canceled
+routes.get('/cancel', (req, res) => {
+  // TODO - handle toast message
+  res.redirect('/');
 });
 
 

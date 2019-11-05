c216c5e469d20010349d584fe4cdac7965b8eb7a
Avery Berkowitz <avery.berkowitz@gmail.com>
2019-11-05 13:03:26 -0600

    fix donation pothole foreign key

--- src/server/db/models/donation.js
+++ src/server/db/models/donation.js
@@ -8,9 +8,5 @@ module.exports = (sequelize, type) => sequelize.define('donation', {
   email: type.STRING, // May need to reference user email as foreign key
   pothole_id: {
     type: type.INTEGER,
-    references: {
-      model: 'pothole', // Note, its table's name, not object name
-      key: 'id', // Note, its a column name
-    }
   },
 });

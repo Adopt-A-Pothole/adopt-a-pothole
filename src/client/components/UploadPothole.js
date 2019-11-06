a74ec6a9f470074991ec5182e1ee3a244c65edce
Avery Berkowitz <avery.berkowitz@gmail.com>
2019-11-05 23:08:05 -0600

    refactor UploadPothole to render image preview

--- src/client/components/UploadPothole.js
+++ src/client/components/UploadPothole.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Image } from 'semantic-ui-react';
 import axios from 'axios';
 
 
 export default class UploadPothole extends Component {
   constructor() {
     super();
+    this.state = {
+      url: null,
+    };
     this.uploadWidget = this.uploadWidget.bind(this);
   }
 
@@ -15,18 +18,26 @@ export default class UploadPothole extends Component {
     // eslint-disable-next-line no-undef
     cloudinary.openUploadWidget({ cloud_name: 'adopt-a-pothole', upload_preset: 'jdsupaox', tags: ['adopt-a-pothole'] },
       (error, result) => {
-        console.log(result[0].secure_url);
+        const url = result[0].secure_url;
+        // render photo
+        this.setState({ url });
       });
   }
 
   render() {
+    const { url } = this.state;
     return (
       <div className="uploadpothole">
-        <div className="upload">
-          <Button onClick={this.uploadWidget} className="upload-button">
-            Add Image
-          </Button>
-        </div>
+        { url ? (
+          <Image src={url} />
+        )
+          : (
+            <div className="upload">
+              <Button onClick={this.uploadWidget} className="upload-button">
+                Add Image
+              </Button>
+            </div>
+          )}
       </div>
     );
   }

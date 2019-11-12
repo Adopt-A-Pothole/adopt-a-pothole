a9dec4acaf1530c6d5f2733379b707545ebf6480
Eliott Frilet <frilet.eliott@gmail.com>
2019-11-12 15:28:38 -0600

    Stub out List component

---
+++ src/client/components/List.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class List extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      potholes: null, // array of all potholes
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        something
+      </div>
+    );
+  }
+}

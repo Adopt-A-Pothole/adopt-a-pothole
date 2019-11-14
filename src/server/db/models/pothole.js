1af70635f73bda5f922918be2db607df8ac2d974
Abel Terefe <abelterefe98@gmail.com>
2019-11-13 23:04:13 -0600

    Added allowedNull option for the fields of zip, median_income, and progress_image. Will change when needed

--- src/server/db/models/pothole.js
+++ src/server/db/models/pothole.js
@@ -1,33 +1,40 @@
-module.exports = (sequelize, type) => {
-  return sequelize.define("pothole", {
-    id: {
-      type: type.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    zip: type.INTEGER, //! <--May need to change it based on api data
-    median_income: type.INTEGER, //! <--May need to change it based on api data
-    longitude: type.DECIMAL(10, 4),
-    latitude: type.DECIMAL(10, 4),
-    severity: type.INTEGER,
-    title: type.STRING,
-    description: type.STRING,
-    fill_cost: type.INTEGER,
-    money_donated: type.DECIMAL(10, 2),
-    filled: type.BOOLEAN,
-    image: type.STRING, // ! represent the orginal pic of the pothole
-    progress_image: type.STRING, // ? <-- y'all like this field name
-    created_at: {
-      type: "TIMESTAMP",
-      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
-      allowNull: false
-    },
-    user_id: {
-      type: type.INTEGER,
-      references: {
-        model: "users",
-        key: "id"
-      }
+module.exports = (sequelize, type) => sequelize.define('pothole', {
+  id: {
+    type: type.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  zip: {
+    type: type.INTEGER,
+    allowNull: true
+  },
+  median_income: { //! <--May need to change it based on api data
+    type: type.INTEGER,
+    allowNull: true
+  },
+  longitude: type.DECIMAL(10, 4),
+  latitude: type.DECIMAL(10, 4),
+  severity: type.INTEGER,
+  title: type.STRING,
+  description: type.STRING,
+  fill_cost: type.INTEGER,
+  money_donated: type.DECIMAL(10, 2),
+  filled: type.BOOLEAN,
+  image: type.STRING, // ! represent the orginal pic of the pothole
+  progress_image: {
+    type: type.STRING,
+    allowNull: true
+  }, // ? <-- y'all like this field name
+  created_at: {
+    type: 'TIMESTAMP',
+    defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+    allowNull: false
+  },
+  user_id: {
+    type: type.INTEGER,
+    references: {
+      model: 'users',
+      key: 'id'
     }
-  });
-};
+  }
+});

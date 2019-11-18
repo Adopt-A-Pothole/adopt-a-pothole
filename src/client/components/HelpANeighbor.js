0566a4d74b4950c12c9a46502af9cae3d07d3f8f
Amber <amber.j.nola@gmail.com>
2019-11-18 09:46:26 -0600

    added a header to help a neighbor

--- src/client/components/HelpANeighbor.js
+++ src/client/components/HelpANeighbor.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Header ,} from 'semantic-ui-react';
 import axios from 'axios';
 import PotholeInNeed from './PotholeInNeed';
 
@@ -65,21 +65,28 @@ export default class HelpANeighbor extends Component {
     if (potholes !== null && potholes.length) { // <-- if not null and the lengeth is greater than 1
       return (
         <div>
-        <Grid columns="four">
-          <Grid.Row>
-            {Object.keys(potholes).map(pothole => (
-              <Grid.Column>
-                <PotholeInNeed
-                  pothole={potholes[pothole]}
-                  donationForm={donationForm}
-                  toggleDonation={this.toggleDonation}
-                  handleDonationInput={this.handleDonationInput}
-                  handleDonation={this.handleDonation}
-                />
-              </Grid.Column>
-            ))}
-          </Grid.Row>
-        </Grid>
+          <h2 className="ui center aligned header">
+            <img src="https://i.imgur.com/ERzSwhh.png"></img>
+            <div className="content">
+              Help A Neighbor
+              <div className="sub header">Contribute to a pothole in your community.</div>
+            </div>
+          </h2>
+          <Grid columns="four">
+            <Grid.Row>
+              {Object.keys(potholes).map(pothole => (
+                <Grid.Column>
+                  <PotholeInNeed
+                    pothole={potholes[pothole]}
+                    donationForm={donationForm}
+                    toggleDonation={this.toggleDonation}
+                    handleDonationInput={this.handleDonationInput}
+                    handleDonation={this.handleDonation}
+                  />
+                </Grid.Column>
+              ))}
+            </Grid.Row>
+          </Grid>
         </div>
       );
     } else {
